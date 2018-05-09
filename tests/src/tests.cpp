6d03a15dccdae6433274300f112fc28c91c49349
Tom Hoxey <tom.hoxey@gmail.com>
2018-05-09 17:04:58 +0100

    added GPU benchmarks

--- tests/src/tests.cpp
+++ tests/src/tests.cpp
@@ -9,31 +9,31 @@ public:
     Q_OBJECT
 
 private slots:
-    void benchmarkCPUbrute();
-    void benchmarkCPUNN();
+//    void benchmarkCPUbrute();
+//    void benchmarkCPUNN();
     void benchmarkGPUbrute();
     void benchmarkGPUNN();
 private:
     uint imageDim = 1024;
-    uint cellCount = 400;
+    uint cellCount = 1000;
 };
 
-void benchmarker::benchmarkCPUbrute()
-{
-    mockCPUSolver cpuBenchmarker;
-    QBENCHMARK
-    {
-        cpuBenchmarker.bruteBenchMark(vec2(imageDim, imageDim), cellCount);
-    }
-}
-void benchmarker::benchmarkCPUNN()
-{
-    mockCPUSolver cpuBenchmarker;
-    QBENCHMARK
-    {
-        cpuBenchmarker.NNBenchMark(vec2(imageDim, imageDim), cellCount);
-    }
-}
+//void benchmarker::benchmarkCPUbrute()
+//{
+//    mockCPUSolver cpuBenchmarker;
+//    QBENCHMARK
+//    {
+//        cpuBenchmarker.bruteBenchMark(vec2(imageDim, imageDim), cellCount);
+//    }
+//}
+//void benchmarker::benchmarkCPUNN()
+//{
+//    mockCPUSolver cpuBenchmarker;
+//    QBENCHMARK
+//    {
+//        cpuBenchmarker.NNBenchMark(vec2(imageDim, imageDim), cellCount);
+//    }
+//}
 void benchmarker::benchmarkGPUbrute()
 {
     mockGPUSolver gpuBenchmarker;
